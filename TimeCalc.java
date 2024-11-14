6aca1f12e1c51cd1300b73deb50f3f7d6ee6b3ac
Omri Ben Atar <omri.benatar@post.runi.ac.il>
2024-11-14 08:32:09 +0200

    TimeCalc test1

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,26 @@
 public class TimeCalc {
     public static void main(String[] args) {
+        //gets a time input which is given using the 24-hour hh:mm format
+        //gets number of minutesToAdd input.
         // Replace this comment with your code
+        String timeInput = args[0];
+        int inputHours = (Integer.parseInt(timeInput.substring(0, 2))) * 60;
+        int inputMinutes = Integer.parseInt(timeInput.substring(3, 5));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totalMinutesToAdd = inputHours + inputMinutes + minutesToAdd;
+        int totalHours = totalMinutesToAdd / 60;
+        totalMinutesToAdd = totalMinutesToAdd % 60;
+        while (totalHours >= 24) {
+            totalHours = totalHours - 24;
+        }
+        String buildHours = "";
+        if(totalHours < 10) {
+            buildHours = "0" + totalHours;
+        } else  buildHours = "" + totalHours;
+        String buildMinutes = "";
+        if (totalMinutesToAdd < 10){
+            buildMinutes = "0" + totalMinutesToAdd;
+        } else buildMinutes = "" + totalMinutesToAdd;
+        System.out.println(buildHours + ":" + buildMinutes);
     }
 }
